418c2571c16eaf5b489b362e04ffee8ae60f806e
rahulch07 <chouguler310@gmail.com>
2026-04-14 20:48:40 +0530

    changes

--- lec1/demo2.py
+++ lec1/demo2.py
@@ -3,3 +3,14 @@ if 0:
 else:
   print("Line 2")
 
+if 3>2:
+  print("Line 3")
+else:
+  print("Line 4")
+
+if 3.14: print("Line 5")
+if "str": print("Line 6")
+
+# if (a = 3+2): print("Line 7")
+
+if (): print("this doesnt work")
---
+++ lec1/demo3.py
@@ -0,0 +1,12 @@
+idx = 0
+while (idx <= 5):
+    print("Hello, World!")
+    idx += 1
+
+lst = ["Apple", "Banana", "Cherry", "Mango", "Grapes"]
+
+for item in lst:
+    print(item)
+
+for val in range(1, 5, 2):
+    print(val)
